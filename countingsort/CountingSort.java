be55fb8af69cd0735de7c3c9680371c0ede4ca4d
Maurício Witter <mwitter@protonmail.com>
2020-07-08 23:16:15 -0300

    fix: comments

--- countingsort/CountingSort.java
+++ countingsort/CountingSort.java
@@ -9,13 +9,13 @@ public class CountingSort {
     int[] count = new int[max + 1]; // aloca memória para o tamanho do maior elemento
 
     for (int i = 0; i < max; ++i)
-      count[i] = 0; // preenche o array do tamanho de maior elemento com zeros
+      count[i] = 0; // preenche o array do tamanho do maior elemento com zeros
 
     for (int i = 0; i < len; i++)
-      count[(data[i] / place) % 10]++; // coloca 1 de 0 a 9 a cada duas posições do array count
+      count[(data[i] / place) % 10]++; // soma 1 ao indíce que é gerado pela divisão e resto da divisão do elemento
 
     for (int i = 1; i < 10; i++)
-      count[i] += count[i - 1]; // conta alguma coisa de 0 a 9 para auxiliar o for abaixo de inserção no output
+      count[i] += count[i - 1]; // faz a contagem de indices para servir a ordenação de output
 
     for (int i = len - 1; i >= 0; i--) {
       /*
