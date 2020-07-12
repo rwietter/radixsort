6656584d7c5c917d0f8ba8794736458e4bb0a854
Maurício Witter <mwitter@protonmail.com>
2020-07-12 00:17:24 -0300

    fix: tdd

--- javascript/__tests__/radix.test.js
+++ javascript/__tests__/radix.test.js
@@ -1,13 +1,19 @@
 const Radix = require("../radixsort.js");
 
-test('sorted array should be [0, 0, 0, 1, 2, 48, 553, 835, 934, 2303, 4994949]', () => {
-  const array = [835, 553, 48, 0, 0, 2303, 934, 4994949, 0, 1, 2];
-  const len = array.length;
-  expect(Radix(array, len)).toMatchObject([0, 0, 0, 1, 2, 48, 553, 835, 934, 2303, 4994949]);
-});
+describe('JavaScrit Radix Sort', () => {
+  test('Radix should return array sorted [0, 0, 0, 1, 2, 48, 553, 835, 934, 2303, 4994949]', () => {
+    const array = [835, 553, 48, 0, 0, 2303, 934, 4994949, 0, 1, 2];
+    const len = array.length;
 
-test('sorted array should be ', () => {
-  const array = [48, 0, 0, 0, 0, 1, 2, 3, 5, 6, 3, 3828, 49,];
-  const len = array.length;
-  expect(Radix(array, len)).toMatchObject([0, 0, 0, 0, 1, 2, 3, 3, 5, 6, 48, 49, 3828]);
+    const expected = [0, 0, 0, 1, 2, 48, 553, 835, 934, 2303, 4994949];
+    expect(Radix(array, len)).toMatchObject(expected);
+  });
+
+  test('Radix should return array sorted [0, 0, 0, 0, 1, 2, 3, 3, 5, 6, 48, 49, 3828]', () => {
+    const array = [48, 0, 0, 0, 0, 1, 2, 3, 5, 6, 3, 3828, 49];
+    const len = array.length;
+
+    const expected = [0, 0, 0, 0, 1, 2, 3, 3, 5, 6, 48, 49, 3828]
+    expect(Radix(array, len)).toMatchObject(expected);
+  });
 });
--- typescript/__tests__/app.test.ts
+++ typescript/__tests__/app.test.ts
@@ -2,18 +2,28 @@ import MaxValue from '../MaxValue'
 import App from '../App';
 import Radixsort from '../Radixsort';
 
-test('array should returns 774', () => {
-  const max = new MaxValue();
-  const arr = [1, 4, 774, 3, 2, 5, 77, 2, 3];
-  expect(max.getMaxValue(arr)).toBe(774);
-});
+describe('TypeScript Radix Sort', () => {
+  test('Max Value of array should return 774', () => {
+    const max = new MaxValue();
+    const arr = [1, 4, 774, 3, 2, 5, 77, 2, 3];
+    
+    const expected = 774;
+    expect(max.getMaxValue(arr)).toBe(expected);
+  });
 
-test('array should returns [1, 2, 2, 3, 3, 4, 5, 77, 774]', () => {
-  const app = new App();
-  const arr = [1, 4, 774, 3, 2, 5, 77, 2, 3];
-  const len = arr.length;
-  const max = app.getMaxValue(arr);
-  const rdx = new Radixsort(arr, len, max);
-  app.radixSort();
-  expect(rdx.radixSort()).toMatchObject([1, 2, 2, 3, 3, 4, 5, 77, 774]);
+  test('Radix Sort should returns array sorted [1, 2, 2, 3, 3, 4, 5, 77, 774]', () => {
+    const app = new App();
+
+    const arr = [1, 4, 774, 3, 2, 5, 77, 2, 3];
+    const len = arr.length;
+
+    const max = app.getMaxValue(arr);
+
+    const rdx = new Radixsort(arr, len, max);
+    app.radixSort();
+
+    const expected: number[] = [1, 2, 2, 3, 3, 4, 5, 77, 774];
+
+    expect(rdx.radixSort()).toMatchObject(expected);
+  });
 });
